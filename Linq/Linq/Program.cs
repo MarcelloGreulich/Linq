ed19484dce7344ffa3a5774e071d77e90a40ae26
Marcello, Greulich <marcello.greulich@tecalliance.net>
2022-09-28 10:30:02 +0200

    Stelle stelle*zahl zahl

--- Linq/Linq/Program.cs
+++ Linq/Linq/Program.cs
@@ -10,13 +10,13 @@ namespace Linq
 
         static void Main(string[] args)
         {
-            string[] cities = { "ROME", "LONDON", "NAIROBI", "CALIFORNIA", "ZURICH", "NEW DELHI", "AMSTERDAM", "ABU DHABI", "PARIS" };
+            int[] nums = new int[] { 5, 1, 9, 2, 3, 7, 4, 5, 6, 8, 7, 6, 3, 4, 5, 2 };;
 
-            var cit = from c in cities
-                      where c.StartsWith("C") && c.EndsWith("A")
-                      select c;
-            foreach (var nu in cit) { 
-                Console.WriteLine(nu);
+            var num = from n in nums
+                      group n by n into p
+                      select p;
+            foreach (var nu in num) { 
+                Console.WriteLine($" {nu.Key}  {nu.Sum()} {nu.Count()} ");
             }
 
             Console.ReadKey();
