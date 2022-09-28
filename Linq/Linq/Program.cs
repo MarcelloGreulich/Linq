0fec138b8371e9098fd4454e78f3e8e40ca4f980
Marcello, Greulich <marcello.greulich@tecalliance.net>
2022-09-28 09:44:59 +0200

    alles durch 2 teilbar ausgeben

---
+++ Linq/Linq/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Linq
+{
+    internal class Program
+    {
+
+
+
+        static void Main(string[] args)
+        {
+            int[] list = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+            for (int i = 0; i < list.Length; i++)
+            {
+                if (list[i] % 2 == 0)
+                {
+                    Console.WriteLine(list[i]);
+                }
+            }
+            Console.ReadKey();
+
+
+        }
+    }
+}
