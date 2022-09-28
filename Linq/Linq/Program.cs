1d63fa885a88eeb77b4db6ab217e1e81712985bd
Marcello, Greulich <marcello.greulich@tecalliance.net>
2022-09-28 14:23:31 +0200

    filetype

--- Linq/Linq/Program.cs
+++ Linq/Linq/Program.cs
@@ -12,95 +12,22 @@ namespace Linq
         static void Main(string[] args)
         {
 
-            List<Students> students = new List<Students>();
-            students.Add(new Students { StuId = 1, StuName = " Joseph ", GrPoint = 800 });
-            students.Add(new Students { StuId = 2, StuName = "Alex", GrPoint = 458 });
-            students.Add(new Students { StuId = 3, StuName = "Harris", GrPoint = 900 });
-            students.Add(new Students { StuId = 4, StuName = "Taylor", GrPoint = 900 });
-            students.Add(new Students { StuId = 5, StuName = "Smith", GrPoint = 458 });
-            students.Add(new Students { StuId = 6, StuName = "Natasa", GrPoint = 700 });
-            students.Add(new Students { StuId = 7, StuName = "David", GrPoint = 750 });
-            students.Add(new Students { StuId = 8, StuName = "Harry", GrPoint = 700 });
-            students.Add(new Students { StuId = 9, StuName = "Nicolash", GrPoint = 597 });
-            students.Add(new Students { StuId = 10, StuName = "Jenny", GrPoint = 750 });
+            string[] arr1 = { "aaa.frx", "bbb.TXT", "xyz.dbf", "abc.pdf", "aaaa.PDF", "xyz.frt", "abc.xml", "ccc.txt", "zzz.txt" };
 
-            int count = Convert.ToInt32(Console.ReadLine());
-            switch (count)
-            {
-                case 1:
-                    count = 900;
-                    break;
-                case 2:
-                    count = 800;
-                    break;
-                case 3:
-                    count = 750;
-                    break;
-                case 4:
-                    count = 700;
-                    break;
-                case 5:
-                    count = 597; 
-                    break;
-                case 6:
-                    count = 458; 
-                    break;
-
-            }
-            var stu = from s in students
-                      group s by s.GrPoint into y
-                      orderby y.Key descending
+            var lis = from a in arr1
+                      group a by a.Split('.')[1] into y
                       select y;
 
-            foreach (var h in stu)
+            string extension;
+            int count;
+            foreach (var a in lis)
             {
-                if (count == h.Key)
-                {
-                    switch (h.Key)
-                    {
-                        case 900:
-                            foreach (var x in h)
-                            {
-                                Console.WriteLine($" Der/Die Schüler/in {x.StuName} hat {x.GrPoint} Punkte");
-                            }
-                            break;
-                        case 800:
-                            foreach (var x in h)
-                            {
-                                Console.WriteLine($" Der/Die Schüler/in {x.StuName} hat {x.GrPoint} Punkte");
-                            }
-                            break;
-                        case 750:
-                            foreach (var x in h)
-                            {
-                                Console.WriteLine($" Der/Die Schüler/in {x.StuName} hat {x.GrPoint} Punkte");
-                            }
-                            break;
-                        case 700:
-                            foreach (var x in h)
-                            {
-                                Console.WriteLine($" Der/Die Schüler/in {x.StuName} hat {x.GrPoint} Punkte");
-                            }
-                            break;
-                        case 597:
-                            foreach (var x in h)
-                            {
-                                Console.WriteLine($" Der/Die Schüler/in {x.StuName} hat {x.GrPoint} Punkte");
-                            }
-                            break;
-                        case 458:
-                            foreach (var x in h)
-                            {
-                                Console.WriteLine($" Der/Die Schüler/in {x.StuName} hat {x.GrPoint} Punkte");
-                            }
-                            break;
+                extension = a.Key;
+                count = a.Count();
 
-                    }
-
-                }
-
-
+                Console.WriteLine($"{count} File(s) with {extension} Extension");
             }
+            
 
 
             Console.ReadKey();
